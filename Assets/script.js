7c3dfff8753a02a97d69d2d9db4b6ba3dc0bc887
Becca Byrne <aggiebec06@gmail.com>
2021-10-24 19:36:46 -0500

    updated files to create quiz content

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,27 @@ var buttonE1 = document.querySelector("#start-quiz");
 buttonE1.addEventListener("click", function() {
 });
 
+
+var questions = [
+    {
+        question: "Which is not a common data type in Javascript?",
+        answer: ["String", "Boolean", "Number", "Property"],
+        correctAnswer: "Property"
+    },
+    {
+        question: "What does DOM stand for?",
+        answer: ["Document Output Model", "Document Output Memory", "Document Object Model", "Document Object Memory"],
+        correctAnswer: "Document Object Model"
+    },
+    {
+        question: "How do you write 'Happy Birthday' in an alert box?",
+        answer: ["prompt('Happy Birthday');", "alertBox('Happy Birthday');", "confirm('Happy Birthday');", "alert('Happy Birthday');"],
+        correctAnswer: "alert('Happy Birthday');"
+    },    
+    { 
+        question: "Arrays in Javascript can be used to store ____.",
+        answer: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        correctAnswer: "All of the above"
+    },
+
+];
