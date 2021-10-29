890c665e2504d284331943b4a2a07e21b25f3ed8
Becca Byrne <aggiebec06@gmail.com>
2021-10-29 11:40:46 -0500

    corrected timer and high scores

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,41 @@
+var questions = [
+    {
+        question: "Which is not a common data type in Javascript?",
+        choices: ["String", "Boolean", "Number", "Property"],
+        answer: "Property",
+    },
+    {
+        question: "What does DOM stand for?",
+        choices: ["Document Output Model", "Document Output Memory", "Document Object Model", "Document Object Memory"],
+        answer: "Document Object Model",
+    },
+    {
+        question: "How do you write 'Happy Birthday' in an alert box?",
+        choices: ["prompt('Happy Birthday');", "alertBox('Happy Birthday');", "msgBox('Happy Birthday');", "alert('Happy Birthday');"],
+        answer: "alert('Happy Birthday');",
+    },    
+    { 
+        question: "Arrays in Javascript can be used to store ____.",
+        choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        answer: "All of the above",
+    },
+    {
+        question: "Which of the following is not a looping structure in Javascript?",
+        choices: ["For", "Loop", "While", "Do...while"],
+        answer: "Loop",
+    }
+];
+
 var startBtn = document.getElementById("start-btn");
 var startScreen = document.getElementById("container");
 var timer = document.getElementById("time");
+var questionContainer = document.getElementById("wrapper");
 var currentQuestion = 0;
 var score = 0;
 var timeLeft = 60;
 var timerId;
 
-
+// starts timer for quiz once start button is clicked
 startBtn.addEventListener("click", function () {
     //timer is starts once the start button is clicked/
     timerId = setInterval(function () {
@@ -19,99 +48,94 @@ startBtn.addEventListener("click", function () {
     }, 1000);
   
     timer.textContent = timeLeft;
-    hideElement();
+    hideElements();
     showQuestion();
   });
 
-
-
-let ranQuestions, currentQuestion
-
-// added event listener to control start button
-startButton.addEventListener("click", startQuiz)
-nextButton.addEventListener("click", () => {
-    currentQuestion++
-    setNextQuestion()
-});
-
-function startQuiz() {
-    startButton.classList.add("hide");
+  // hides intro and start button, removes hide from timer
+function hideElements() {
+    startBtn.classList.add("hide");
     document.getElementById("intro").classList.add("hide");
-    ranQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestion = 0;
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion();
+    document.getElementById("count").classList.remove("hide");
+}
+
+// div is created for questions
+var displayQuestion = document.createElement("div");
+    displayQuestion.setAttribute("id", "questions");
+    questionContainer.appendChild(displayQuestion);
+
+// score being displayed
+var answers = document.createElement("div");
+answers.setAttribute("id", "answers");
+questionContainer.appendChild(answers);
+
+//display questions
+function showQuestion() {
+  
+// console.log(questions[currentQuestion].question);
+displayQuestion.textContent = questions[currentQuestion].question;
+  
+for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = questions[currentQuestion].choices[i];
+    choiceBtn.setAttribute("class", "btn");
+    choiceBtn.setAttribute("value", questions[currentQuestion].choices[i]);
+    displayQuestion.append(choiceBtn);
+
+    choiceBtn.onclick = buttonClick;
+    }
+  
+//selecting an answer
+function buttonClick() {
+    if (this.value === questions[currentQuestion].answer) {
+        score++;
+    } else if (this.value !== questions[currentQuestion].answer) {
+        timeLeft -= 10;
+        timer.textContent = timeLeft;
+    }
+  
+currentQuestion++;
+  
+if (currentQuestion === questions.length) {
+endGame();
+} else {
+showQuestion();
+}
+answers.textContent = "Total Correct: " + score + "/5";
+}
+}
+  
+  // if time runs out or user answers all questions the game is ended
+function endGame() {
+    displayQuestion.textContent = "";
+    var endScreen = document.getElementById("end-screen");
+    endScreen.classList.remove("hide");
+    clearInterval(timerId);
+  
+var finalScore = document.getElementById("final-score");
+finalScore.textContent = timeLeft;
+  
+}
+  
+function saveScore() {
+var initialInput = document.getElementById("initials").value.trim();
+  
+// getting the highScore key
+var scores = JSON.parse(localStorage.getItem("highScore")) || [];
+  
+var newScore = {
+    score: timeLeft,
+    initals: initialInput,
+    correct: score,
 };
-
-
-function setNextQuestion() {
-    resetState()
-    showQuestion(ranQuestions[currentQuestion]);
+  
+//adds data the the newScore variable
+scores.push(newScore);
+// sets the highScore key
+localStorage.setItem("highScore", JSON.stringify(scores));
 }
-
-function showQuestion(questions) {
-    questionIndex.innerText = questions.question;
-    questions.answers.forEach(answers => {
-        const button = document.createElement("button");
-        button.innerText = answers.text;
-        button.classList.add("btn");
-        if (answers.correct) {
-            button.dataset.correct = answers.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-        answerEl.appendChild(button);
-    });
-}
-
-function resetState() {
-    nextButton.classList.add('hide');
-    while (answerEl.firstChild) {
-        answerEl.removeChild(answerEl.firstChild)
-    }
-}
-function selectAnswer(event) {
-    const selectedButton = event.target;
-    const correct = selectedButton.dataset.correct;
-    if (ranQuestions.length > currentQuestion +1) {
-        nextButton.classList.remove('hide');
-    } else {
-        startButton.innerHTML = "Restart"
-        startButton.classList.remove('hide');
-    }
-    
- }
-
-var questions = [
-    {
-        question: "Which is not a common data type in Javascript?",
-        answers: [
-            {text: "String", correct: false},
-            {text: "Boolean", correct: false},
-            {text: "Number", correct: false},
-            {text: "Property", correct: true}
-        ]},
-    {
-        question: "What does DOM stand for?",
-        answers: [
-            {text: "Document Output Model", correct: false},
-            {text: "Document Output Memory", correct: false},
-            {text: "Document Object Model", correct: true},
-            {text: "Document Object Memory", correct: false}
-        ]},
-    {
-        question: "How do you write 'Happy Birthday' in an alert box?",
-        answers: [
-            {text:"prompt('Happy Birthday');", correct: false},
-            {text:"alertBox('Happy Birthday');", correct: false},
-            {text:"msgBox('Happy Birthday');", correct: false},
-            {text:"alert('Happy Birthday');", correct: true}
-       ]},    
-    { 
-        question: "Arrays in Javascript can be used to store ____.",
-        answers: [
-            {text:"Numbers and strings", correct: false},
-            {text: "Other arrays", correct: false}, 
-            {text: "Booleans", correct: false},
-            {text: "All of the above", correct: true}
-        ]},
-];
+  
+// when the submit button is clicked saveScore function is ran to store data
+var SubmitBtn = document.getElementById("submit");
+SubmitBtn.addEventListener("click", saveScore);
+  
